d3d1eccf5aa70fa6d518d3d6952c7a38ca4251a6
DJ Aaron <koreangeekman@gmail.com>
2023-09-28 16:27:36 -0600

    base requirements met

--- script.js
+++ script.js
@@ -5,13 +5,17 @@ const heroStats = [
     HP: 100,
     Lvl: 1,
     Gold: 20,
+    XP: 12,
+    xpMultiplier: 4
   },
   {
     name: 'Zelda',
     damage: 10,
     HP: 100,
     Lvl: 2,
-    Gold: 100
+    Gold: 100,
+    XP: 20,
+    xpMultiplier: 3
   }
 ]
 
@@ -19,8 +23,17 @@ const bossStats = {
   health: 100,
   maxHealth: 100,
   damage: 5,
-  level: 1
+  level: 1,
+  deaths: 0,
+  reward: 10
 }
+
+const shopItems = {
+  potion: 20,
+  tingle: 1,
+  epona: 500
+}
+
 
 
 function attackBoss() {
@@ -29,8 +42,25 @@ function attackBoss() {
   bossStats.health -= heroDamage
   if (bossStats.health <= 0) {
     window.alert('You Win, Ganon Leveled UP!')
+
     bossStats.level++
     bossStats.health = 100 * bossStats.level
+    bossStats.deaths++
+    heroStats.forEach(hero => {
+
+      hero.Gold += bossStats.reward
+      hero.XP += 1 * hero.xpMultiplier
+      hero.Lvl = Math.floor(hero.XP / 10)
+
+
+    })
+
+    drawHero()
+    bossStats.reward += 10
+
+    document.getElementById('bossLevel').innerText = bossStats.level
+    document.getElementById('bossKills').innerText = bossStats.deaths
+    document.getElementById('bossReward').innerText = bossStats.reward
   }
   document.getElementById('bossHealth').innerText = bossStats.health
 
@@ -51,10 +81,59 @@ function bossAttacks() {
 function drawHero() {
   const LinkHP = heroStats.find(hero => hero.name == 'Link')
   document.getElementById('linkHP').innerText = LinkHP.HP
+  document.getElementById('linkGOLD').innerText = LinkHP.Gold
+  document.getElementById('linkLVL').innerText = LinkHP.Lvl
   const ZeldaHP = heroStats.find(hero => hero.name == 'Zelda')
   document.getElementById('zeldaHP').innerText = ZeldaHP.HP
-
-
+  document.getElementById('zeldaGOLD').innerText = ZeldaHP.Gold
+  document.getElementById('zeldaLVL').innerText = ZeldaHP.Lvl
 }
+
+
+
+function healButton() {
+  const LinkHP = heroStats.find(hero => hero.name == 'Link')
+  const ZeldaHP = heroStats.find(hero => hero.name == 'Zelda')
+
+
+  Swal.fire({
+    title: 'Who would you like to heal?',
+    showConfirmButton: (LinkHP.Gold >= 20),
+    showDenyButton: (ZeldaHP.Gold >= 20),
+    showCancelButton: true,
+    confirmButtonText: 'Link',
+    denyButtonText: `Zelda`,
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+
+    if (result.isConfirmed) {
+      Swal.fire('Link Healed!', '', 'success')
+
+      LinkHP.HP += 50
+      LinkHP.Gold -= 20
+      if (LinkHP.HP >= 100) {
+        LinkHP.HP = 100
+      }
+
+
+
+
+    } else if (result.isDenied) {
+      Swal.fire('Zelda Healed', '', 'success')
+
+      ZeldaHP.HP += 50
+      ZeldaHP.Gold -= 20
+      if (ZeldaHP.HP >= 100) {
+        ZeldaHP.HP = 100
+      }
+
+    }
+  })
+}
+
+
+
+
+drawHero()
 
 setInterval(bossAttacks, 1000)
