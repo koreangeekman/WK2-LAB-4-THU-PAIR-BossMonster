5629e1b8f2e25fd3ca5b0e5cb5d857c49ffcd1ff
DJ Aaron <koreangeekman@gmail.com>
2023-09-28 18:21:24 -0600

    minor tweaks + refactoring

--- script.js
+++ script.js
@@ -34,43 +34,62 @@ const shopItems = {
   epona: 500
 }
 
-
-
 function attackBoss() {
   let heroDamage = 0
-  heroStats.forEach(hero => heroDamage += hero.damage)
+  heroStats.forEach(hero => {
+    if (hero.HP > 0) {
+      heroDamage += hero.damage
+    }
+  })
+
   bossStats.health -= heroDamage
   if (bossStats.health <= 0) {
-    window.alert('You Win, Ganon Leveled UP!')
-
+    window.alert('You Win! But Ganon Leveled UP!')
+    bossStats.deaths++
     bossStats.level++
     bossStats.health = 100 * bossStats.level
-    bossStats.deaths++
-    heroStats.forEach(hero => {
-
-      hero.Gold += bossStats.reward
-      hero.XP += 1 * hero.xpMultiplier
-      hero.Lvl = Math.floor(hero.XP / 10)
-
-
-    })
-
-    drawHero()
+    awardXP()
     bossStats.reward += 10
-
-    document.getElementById('bossLevel').innerText = bossStats.level
-    document.getElementById('bossKills').innerText = bossStats.deaths
-    document.getElementById('bossReward').innerText = bossStats.reward
   }
-  document.getElementById('bossHealth').innerText = bossStats.health
+  drawBoss()
+}
 
+function awardXP() {
+  heroStats.forEach(hero => {
+    hero.Gold += bossStats.reward
+    hero.XP += 1 * hero.xpMultiplier
+    hero.Lvl = Math.floor(hero.XP / 10)
+  })
+  drawHero()
+}
+
+function drawBoss() {
+  document.getElementById('bossHealth').innerText = bossStats.health
+  document.getElementById('bossLevel').innerText = bossStats.level
+  document.getElementById('bossKills').innerText = bossStats.deaths
+  document.getElementById('bossReward').innerText = bossStats.reward
+
+}
+
+function drawHero() {
+  const heroLink = heroStats.find(hero => hero.name == 'Link')
+  document.getElementById('linkHP').innerText = heroLink.HP
+  document.getElementById('linkGOLD').innerText = heroLink.Gold
+  document.getElementById('linkLVL').innerText = heroLink.Lvl
+  const heroZelda = heroStats.find(hero => hero.name == 'Zelda')
+  document.getElementById('zeldaHP').innerText = heroZelda.HP
+  document.getElementById('zeldaGOLD').innerText = heroZelda.Gold
+  document.getElementById('zeldaLVL').innerText = heroZelda.Lvl
 }
 
 function bossAttacks() {
   // heroStats.forEach(hero => hero.HP -= bossStats.damage)
-  const Rng = Math.floor(Math.random() * heroStats.length)
-  heroStats[Rng].HP -= bossStats.damage
 
+  //RNG for who to attack
+  const Rng = Math.floor(Math.random() * heroStats.length)
+  // Boss damages Hero
+  heroStats[Rng].HP -= bossStats.damage
+  // prevent negative health
   if (heroStats[Rng].HP <= 0) {
     heroStats[Rng].HP = 0
   }
@@ -78,61 +97,37 @@ function bossAttacks() {
   drawHero()
 }
 
-function drawHero() {
-  const LinkHP = heroStats.find(hero => hero.name == 'Link')
-  document.getElementById('linkHP').innerText = LinkHP.HP
-  document.getElementById('linkGOLD').innerText = LinkHP.Gold
-  document.getElementById('linkLVL').innerText = LinkHP.Lvl
-  const ZeldaHP = heroStats.find(hero => hero.name == 'Zelda')
-  document.getElementById('zeldaHP').innerText = ZeldaHP.HP
-  document.getElementById('zeldaGOLD').innerText = ZeldaHP.Gold
-  document.getElementById('zeldaLVL').innerText = ZeldaHP.Lvl
-}
-
-
 
 function healButton() {
-  const LinkHP = heroStats.find(hero => hero.name == 'Link')
-  const ZeldaHP = heroStats.find(hero => hero.name == 'Zelda')
+  const heroLink = heroStats.find(hero => hero.name == 'Link')
+  const heroZelda = heroStats.find(hero => hero.name == 'Zelda')
 
-
+  // replaced isConfirmed with Link & isDenied with Zelda
   Swal.fire({
     title: 'Who would you like to heal?',
-    showConfirmButton: (LinkHP.Gold >= 20),
-    showDenyButton: (ZeldaHP.Gold >= 20),
+    showConfirmButton: (heroLink.Gold >= 20 && heroLink.HP > 0),
+    showDenyButton: (heroZelda.Gold >= 20 && heroZelda.HP > 0),
     showCancelButton: true,
     confirmButtonText: 'Link',
     denyButtonText: `Zelda`,
   }).then((result) => {
-    /* Read more about isConfirmed, isDenied below */
-
     if (result.isConfirmed) {
-      Swal.fire('Link Healed!', '', 'success')
-
-      LinkHP.HP += 50
-      LinkHP.Gold -= 20
-      if (LinkHP.HP >= 100) {
-        LinkHP.HP = 100
+      Swal.fire('Link Healed!', '-20 Gold from Link', 'success')
+      heroLink.HP += 50
+      heroLink.Gold -= 20
+      if (heroLink.HP >= 100) {
+        heroLink.HP = 100
       }
-
-
-
-
     } else if (result.isDenied) {
-      Swal.fire('Zelda Healed', '', 'success')
-
-      ZeldaHP.HP += 50
-      ZeldaHP.Gold -= 20
-      if (ZeldaHP.HP >= 100) {
-        ZeldaHP.HP = 100
+      Swal.fire('Zelda Healed', '-20 Gold from Zelda', 'success')
+      heroZelda.HP += 50
+      heroZelda.Gold -= 20
+      if (heroZelda.HP >= 100) {
+        heroZelda.HP = 100
       }
-
     }
   })
 }
-
-
-
 
 drawHero()
 
